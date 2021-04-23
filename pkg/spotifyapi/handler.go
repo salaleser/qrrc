76b733d6d6047373386b4d7b5715695db82c0b11
salaleser <salaleser@yandex.ru>
2021-04-23 23:55:04 +0300

    Auto activate the first device

--- pkg/spotifyapi/handler.go
+++ pkg/spotifyapi/handler.go
@@ -48,6 +48,12 @@ func CompleteAuthHandler(w http.ResponseWriter, r *http.Request) {
 		ch <- &client
 	}
 
+	err := activateFirstDevice(w)
+	if err != nil {
+		handleError(w, err, "get track")
+		return
+	}
+
 	loadPage(w, "home", []string{"text", "toggle_play"},
 		[]string{"Успех! Теперь можешь управлять спотифаем или поиграть в " +
 			"угадаечку.", "Toggle Play/Pause"})
