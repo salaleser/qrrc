eca3722731e73e1070a75758a3b83348afe36be5
salaleser <salaleser@yandex.ru>
2021-10-10 15:22:34 +0200

    WIP

---
+++ static/scripts/script.js
@@ -0,0 +1,42 @@
+function answer() {
+    document.location.href = "/spotify/nonamegamego/answer"
+}
+
+function answer0() {
+    document.location.href = "/spotify/nonamegamego/main?correct=-50"
+}
+
+function answer1() {
+    document.location.href = "/spotify/nonamegamego/main?correct=50"
+}
+
+function answer2() {
+    document.location.href = "/spotify/nonamegamego/main?correct=100"
+}
+
+function exit() {
+    document.location.href = "/spotify/home"
+}
+
+function start() {
+    const names = []
+    const c = document.getElementsByClassName('name')
+    for (let i = 0; i < c.length; i++) {
+        const v = c.item(i).value
+        if (v !== '') {
+            names.push(v)
+        }
+    }
+
+    if (names.length < 2) {
+        alert('Укажите более одного игрока')
+        return
+    }
+
+    document.location.href = "/spotify/nonamegamego/main?start=true&player_names=" + names.join(',')
+}   
+
+function setPlaylist() {
+    const v = document.getElementById('playlist').value
+    document.location.href = `/spotify/nonamegamego/setup?playlist=${v}`
+}
